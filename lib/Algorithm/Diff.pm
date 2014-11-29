7469b14af691be5760e0716afe3daa7b2938df9f
Tye McQueen <tyemq@cpan.org>
2014-11-29 14:18:51 -0800

    Whitespace in code.

--- lib/Algorithm/Diff.pm
+++ lib/Algorithm/Diff.pm
@@ -328,113 +328,118 @@ minor improvements to improve the speed.
 =cut
 
 
-sub LCS_matrix {
-  my @x;
-  my $a;				# Sequence #1
-  my $b;				# Sequence #2
+sub LCS_matrix
+{
+	my @x;
+	my $a;				# Sequence #1
+	my $b;				# Sequence #2
 
-  $a = shift or usage();
-  $b = shift or usage();
-  (ref $a eq 'ARRAY') or usage();
-  (ref $b eq 'ARRAY') or usage();
-  my $eq = shift;
-  
-  my ($al, $bl);			# Lengths of sequences
-  $al = @$a;
-  $bl = @$b;
+	$a = shift or usage();
+	$b = shift or usage();
+	(ref $a eq 'ARRAY') or usage();
+	(ref $b eq 'ARRAY') or usage();
+	my $eq = shift;
+	
+	my ($al, $bl);			# Lengths of sequences
+	$al = @$a;
+	$bl = @$b;
 
-  my ($i, $j);
+	my ($i, $j);
 
-  $x[0] = [(0) x ($bl+1)];
-  for ($i=1; $i<=$al; $i++) {
-    my $r = $x[$i] = [];
-    $r->[0] = 0;
-    for ($j=1; $j<=$bl; $j++) {
-      # If the first two items are the same...
-      if (defined $eq 
-	  ? $eq->($a->[-$i], $b->[-$j])
-	  : $a->[-$i] eq $b->[-$j]
-	 ) { 
-	$r->[$j] = 1 + $x[$i-1][$j-1];
-      } else {
-	my $pi = $x[$i][$j-1];
-	my $pj = $x[$i-1][$j];
-	$r->[$j] = ($pi > $pj ? $pi : $pj);
-      }
-    }
-  }
+	$x[0] = [(0) x ($bl+1)];
+	for ($i=1; $i<=$al; $i++) {
+	  my $r = $x[$i] = [];
+	  $r->[0] = 0;
+	  for ($j=1; $j<=$bl; $j++) {
+		# If the first two items are the same...
+		if (defined $eq 
+		? $eq->($a->[-$i], $b->[-$j])
+		: $a->[-$i] eq $b->[-$j]
+	   ) { 
+	  $r->[$j] = 1 + $x[$i-1][$j-1];
+		} else {
+	  my $pi = $x[$i][$j-1];
+	  my $pj = $x[$i-1][$j];
+	  $r->[$j] = ($pi > $pj ? $pi : $pj);
+		}
+	  }
+	}
 
-  \@x;
+	\@x;
 }
 
-sub traverse_sequences {
-  my $dispatcher = shift;
-  my $a = shift;
-  my $b = shift;
-  my $equal = shift;
-  my $x = LCS_matrix($a, $b, $equal);
+sub traverse_sequences
+{
+	my $dispatcher = shift;
+	my $a = shift;	# array ref
+	my $b = shift;	# array ref
+	my $equal = shift;
+	my $x = LCS_matrix($a, $b, $equal);
 
-  my ($al, $bl) = (scalar(@$x)-1, scalar(@{$x->[0]})-1);
-  my ($ap, $bp) = ($al, $bl);
-  my $dispf;
-  while (1) {
-    $dispf = undef;
-    my ($ai, $bi) = ($al-$ap, $bl-$bp);
-    if ($ap == 0) {
-      $dispf = $dispatcher->{A_FINISHED} || $dispatcher->{DISCARD_B};
-      $bp--;			# Where to put this?
-    } elsif ($bp == 0) {
-      $dispf = $dispatcher->{B_FINISHED} || $dispatcher->{DISCARD_A};
-      $ap--;			# Where to put this?
-    } elsif (defined($equal) 
-	     ? $equal->($a->[$ai], $b->[$bi])
-	     : $a->[$ai] eq $b->[$bi]
-	    ) {
-      $dispf = $dispatcher->{MATCH};
-      $ap--; 
+	my ($al, $bl) = (scalar(@$x)-1, scalar(@{$x->[0]})-1);
+	my ($ap, $bp) = ($al, $bl);
+	my $dispf;
+	while (1) {
+      $dispf = undef;
+      my ($ai, $bi) = ($al-$ap, $bl-$bp);
+      if ($ap == 0) {
+        $dispf = $dispatcher->{A_FINISHED} || $dispatcher->{DISCARD_B};
+        $bp--;			# Where to put this?
+      } elsif ($bp == 0) {
+        $dispf = $dispatcher->{B_FINISHED} || $dispatcher->{DISCARD_A};
+        $ap--;			# Where to put this?
+      } elsif (defined($equal) 
+           ? $equal->($a->[$ai], $b->[$bi])
+           : $a->[$ai] eq $b->[$bi]
+          ) {
+        $dispf = $dispatcher->{MATCH};
+        $ap--; 
+        $bp--;
+      } else {
+        if ($x->[$ap][$bp] == $x->[$ap-1][$bp] + 1) {
+      $dispf = $dispatcher->{DISCARD_B};
       $bp--;
-    } else {
-      if ($x->[$ap][$bp] == $x->[$ap-1][$bp] + 1) {
-	$dispf = $dispatcher->{DISCARD_B};
-	$bp--;
-      } else {
-	$dispf = $dispatcher->{DISCARD_A};
-	$ap--;
+        } else {
+      $dispf = $dispatcher->{DISCARD_A};
+      $ap--;
+        }
       }
-    }
-    $dispf->($ai, $bi, @_) if defined $dispf;
-    return 1 if $ap == 0 && $bp == 0;
-  }
+      $dispf->($ai, $bi, @_) if defined $dispf;
+      return 1 if $ap == 0 && $bp == 0;
+	}
 }
 
-sub LCS {
-  my $lcs = [];
-  my ($a, $b) = @_;
-  my $functions = { MATCH => sub {push @$lcs, $a->[$_[0]]} };
-  
-  traverse_sequences($functions, @_);
-  wantarray ? @$lcs : $lcs;
+sub LCS
+{
+	my $lcs = [];
+	my ($a, $b) = @_;
+	my $functions = { MATCH => sub {push @$lcs, $a->[$_[0]]} };
+	
+	traverse_sequences($functions, @_);
+	wantarray ? @$lcs : $lcs;
 }
 
-sub diff {
-  my ($a, $b) = @_;
-  my @cur_diff = ();
-  my @diffs = ();
+sub diff
+{
+	my ($a, $b) = @_;
+	my @cur_diff = ();
+	my @diffs = ();
 
-  my $functions =
-    { DISCARD_A => sub {push @cur_diff, ['-', $_[0], $a->[$_[0]]]},
-      DISCARD_B => sub {push @cur_diff, ['+', $_[1], $b->[$_[1]]]},
-      MATCH => sub { push @diffs, [@cur_diff] if @cur_diff; 
-		     @cur_diff = ()
-		   },
-    };
+	my $functions =
+	  { DISCARD_A => sub {push @cur_diff, ['-', $_[0], $a->[$_[0]]]},
+		DISCARD_B => sub {push @cur_diff, ['+', $_[1], $b->[$_[1]]]},
+		MATCH => sub { push @diffs, [@cur_diff] if @cur_diff; 
+			   @cur_diff = ()
+			 },
+	  };
 
-  traverse_sequences($functions, @_);
-  push @diffs, \@cur_diff if @cur_diff;
-  wantarray ? @diffs : \@diffs;
+	traverse_sequences($functions, @_);
+	push @diffs, \@cur_diff if @cur_diff;
+	wantarray ? @diffs : \@diffs;
 }
 
-sub usage {
+sub usage
+{
   require Carp;
   Carp::croak("Usage: LCS([...], [...]); aborting");
 }
