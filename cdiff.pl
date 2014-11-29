f5fe4c8482fd75e1f5c988a9ba57ab8d736a0de9
Tye McQueen <tyemq@cpan.org>
2014-11-29 12:19:00 -0800

    Update credit for cpm in cdiff.pl.

--- cdiff.pl
+++ cdiff.pl
@@ -6,7 +6,8 @@
 # This program is free software; you can redistribute it and/or modify it
 # under the same terms as Perl itself.
 #
-# hacked to do context diffs, cpm sep 1998
+# Altered to output in `context diff' format (but without context)
+# September 1998 Christian Murphy (cpm@muc.de)
 #
 use strict;
 
