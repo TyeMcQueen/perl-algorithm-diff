6b97d0902ea9d4e07ef332bc19128b195a902a4d
Tye McQueen <tyemq@cpan.org>
2014-11-29 18:39:55 -0800

    Update POD for DiffOld.

--- lib/Algorithm/DiffOld.pm
+++ lib/Algorithm/DiffOld.pm
@@ -1,4 +1,8 @@
-package Algorithm::Diff;
+# This is a version of Algorithm::Diff that uses only a comparison function,
+# like versions <= 0.59 used to.
+# $Revision: 1.1 $
+
+package Algorithm::DiffOld;
 use strict;
 use vars qw($VERSION @EXPORT_OK @ISA @EXPORT);
 use integer;		# see below in _replaceNextLargerWith() for mod to make
@@ -7,7 +11,7 @@ require Exporter;
 @ISA = qw(Exporter);
 @EXPORT = qw();
 @EXPORT_OK = qw(LCS diff traverse_sequences);
-$VERSION = sprintf('%d.%02d', (q$Revision: 1.10 $ =~ /\d+/g));
+$VERSION = 1.10;	# manually tracking Algorithm::Diff
 
 # McIlroy-Hunt diff algorithm
 # Adapted from the Smalltalk code of Mario I. Wolczko, <mario@wolczko.com>
@@ -15,317 +19,69 @@ $VERSION = sprintf('%d.%02d', (q$Revision: 1.10 $ =~ /\d+/g));
 
 =head1 NAME
 
-Algorithm::Diff - Compute `intelligent' differences between two files / lists
+Algorithm::DiffOld - Compute `intelligent' differences between two files / lists
+but use the old (<=0.59) interface.
+
+=head1 NOTE
+
+This has been provided as part of the Algorithm::Diff package by Ned Konz.
+This particular module is B<ONLY> for people who B<HAVE> to have the old
+interface, which uses a comparison function rather than a key generating
+function.
+
+Because each of the lines in one array have to be compared with each 
+of the lines in the other array, this does M*N comparisions. This can
+be very slow. I clocked it at taking 18 times as long as the stock
+version of Algorithm::Diff for a 4000-line file. It will get worse
+quadratically as array sizes increase.
 
 =head1 SYNOPSIS
 
-  use Algorithm::Diff qw(diff LCS traverse_sequences);
+  use Algorithm::DiffOld qw(diff LCS traverse_sequences);
 
-  @lcs    = LCS( \@seq1, \@seq2 );
+  @lcs    = LCS( \@seq1, \@seq2, $comparison_function );
 
-  @lcs    = LCS( \@seq1, \@seq2, $key_generation_function );
+  $lcsref = LCS( \@seq1, \@seq2, $comparison_function );
 
-  $lcsref = LCS( \@seq1, \@seq2 );
-
-  $lcsref = LCS( \@seq1, \@seq2, $key_generation_function );
-
-  @diffs = diff( \@seq1, \@seq2 );
-
-  @diffs = diff( \@seq1, \@seq2, $key_generation_function );
+  @diffs = diff( \@seq1, \@seq2, $comparison_function );
   
   traverse_sequences( \@seq1, \@seq2,
                      { MATCH => $callback,
                        DISCARD_A => $callback,
                        DISCARD_B => $callback,
-                     } );
-
-  traverse_sequences( \@seq1, \@seq2,
-                     { MATCH => $callback,
-                       DISCARD_A => $callback,
-                       DISCARD_B => $callback,
                      },
-                     $key_generation_function );
+                     $comparison_function );
 
-=head1 INTRODUCTION
+=head1 COMPARISON FUNCTIONS
 
-(by Mark-Jason Dominus)
+Each of the main routines should be passed a comparison function. If you
+aren't passing one in, B<use Algorithm::Diff instead>.
 
-I once read an article written by the authors of C<diff>; they said
-that they hard worked very hard on the algorithm until they found the
-right one.
+These functions should return a true value when two items should compare
+as equal.
 
-I think what they ended up using (and I hope someone will correct me,
-because I am not very confident about this) was the `longest common
-subsequence' method.  in the LCS problem, you have two sequences of
-items:
+For instance,
 
-        a b c d f g h j q z
+  @lcs    = LCS( \@seq1, \@seq2, sub { my ($a, $b) = @_; $a eq $b } );
 
-        a b c d e f g i j k r x y z
+but if that is all you're doing with your comparison function, just use
+Algorithm::Diff and let it do this (this is its default).
 
-and you want to find the longest sequence of items that is present in
-both original sequences in the same order.  That is, you want to find
-a new sequence I<S> which can be obtained from the first sequence by
-deleting some items, and from the secend sequence by deleting other
-items.  You also want I<S> to be as long as possible.  In this case
-I<S> is
+Or:
 
-        a b c d f g j z
-
-From there it's only a small step to get diff-like output:
-
-        e   h i   k   q r x y 
-        +   - +   +   - + + +
-
-This module solves the LCS problem.  It also includes a canned
-function to generate C<diff>-like output.
-
-It might seem from the example above that the LCS of two sequences is
-always pretty obvious, but that's not always the case, especially when
-the two sequences have many repeated elements.  For example, consider
-
-	a x b y c z p d q
-	a b c a x b y c z
-
-A naive approach might start by matching up the C<a> and C<b> that
-appear at the beginning of each sequence, like this:
-
-	a x b y c         z p d q
-	a   b   c a b y c z
-
-This finds the common subsequence C<a b c z>.  But actually, the LCS
-is C<a x b y c z>:
-
-	      a x b y c z p d q
-	a b c a x b y c z
-
-=head1 USAGE
-
-This module provides three exportable functions, which we'll deal with in
-ascending order of difficulty: C<LCS>, C<diff>, and
-C<traverse_sequences>.
-
-=head2 C<LCS>
-
-Given references to two lists of items, LCS returns an array containing their
-longest common subsequence.  In scalar context, it returns a reference to
-such a list.
-
-  @lcs    = LCS( \@seq1, \@seq2 );
-  $lcsref = LCS( \@seq1, \@seq2 );
-
-C<LCS> may be passed an optional third parameter; this is a CODE
-reference to a key generation function.  See L</KEY GENERATION
-FUNCTIONS>.
-
-  @lcs    = LCS( \@seq1, \@seq2, $keyGen );
-  $lcsref = LCS( \@seq1, \@seq2, $keyGen );
-
-Additional parameters, if any, will be passed to the key generation
-routine.
-
-=head2 C<diff>
-
-  @diffs     = diff( \@seq1, \@seq2 );
-  $diffs_ref = diff( \@seq1, \@seq2 );
-
-C<diff> computes the smallest set of additions and deletions necessary
-to turn the first sequence into the second, and returns a description
-of these changes.  The description is a list of I<hunks>; each hunk
-represents a contiguous section of items which should be added,
-deleted, or replaced.  The return value of C<diff> is a list of
-hunks, or, in scalar context, a reference to such a list.
-
-Here is an example:  The diff of the following two sequences:
-
-  a b c e h j l m n p
-  b c d e f j k l m r s t
-
-Result:
-
- [ 
-   [ [ '-', 0, 'a' ] ],       
-
-   [ [ '+', 2, 'd' ] ],
-
-   [ [ '-', 4, 'h' ] , 
-     [ '+', 4, 'f' ] ],
-
-   [ [ '+', 6, 'k' ] ],
-
-   [ [ '-', 8, 'n' ], 
-     [ '-', 9, 'p' ], 
-     [ '+', 9, 'r' ], 
-     [ '+', 10, 's' ], 
-     [ '+', 11, 't' ],
-   ]
- ]
-
-There are five hunks here.  The first hunk says that the C<a> at
-position 0 of the first sequence should be deleted (C<->).  The second
-hunk says that the C<d> at position 2 of the second sequence should
-be inserted (C<+>).  The third hunk says that the C<h> at position 4
-of the first sequence should be removed and replaced with the C<f>
-from position 4 of the second sequence.  The other two hunks similarly. 
-
-C<diff> may be passed an optional third parameter; this is a CODE
-reference to a key generation function.  See L</KEY GENERATION
-FUNCTIONS>.
-
-Additional parameters, if any, will be passed to the key generation
-routine.
-
-=head2 C<traverse_sequences>
-
-C<traverse_sequences> is the most general facility provided by this
-module; C<diff> and C<LCS> are implemented as calls to it.
-
-Imagine that there are two arrows.  Arrow A points to an element of
-sequence A, and arrow B points to an element of the sequence B.
-Initially, the arrows point to the first elements of the respective
-sequences.  C<traverse_sequences> will advance the arrows through the
-sequences one element at a time, calling an appropriate user-specified
-callback function before each advance.  It willadvance the arrows in
-such a way that if there are equal elements C<$A[$i]> and C<$B[$j]>
-which are equal and which are part of the LCS, there will be some
-moment during the execution of C<traverse_sequences> when arrow A is
-pointing to C<$A[$i]> and arrow B is pointing to C<$B[$j]>.  When this
-happens, C<traverse_sequences> will call the C<MATCH> callback
-function and then it will advance both arrows. 
-
-Otherwise, one of the arrows is pointing to an element of its sequence
-that is not part of the LCS.  C<traverse_sequences> will advance that
-arrow and will call the C<DISCARD_A> or the C<DISCARD_B> callback,
-depending on which arrow it advanced.  If both arrows point to
-elements that are not part of the LCS, then C<traverse_sequences> will
-advance one of them and call the appropriate callback, but it is not
-specified which it will call.
-
-The arguments to C<traverse_sequences> are the two sequences to
-traverse, and a callback which specifies the callback functions, like
-this:
-
-  traverse_sequences( \@seq1, \@seq2,
-                     { MATCH => $callback_1,
-                       DISCARD_A => $callback_2,
-                       DISCARD_B => $callback_3,
-                     } );
-
-Callbacks are invoked with at least the indices of the two arrows as
-their arguments.  They are not expected to return any values.  If a
-callback is omitted from the table, it is not called.
-
-If arrow A reaches the end of its sequence, before arrow B does,
-C<traverse_sequences> will call the C<A_FINISHED> callback when it
-advances arrow B, if there is such a function; if not it will call
-C<DISCARD_B> instead.  Similarly if arrow B finishes first.
-C<traverse_sequences> returns when both arrows are at the ends of
-their respective sequences.  It returns true on success and false on
-failure.  At present there is no way to fail.
-
-C<traverse_sequences> may be passed an optional fourth parameter; this
-is a CODE reference to a key generation function.  See L</KEY
-GENERATION FUNCTIONS>.
-
-Additional parameters, if any, will be passed to the key generation
-function.
-
-=head1 KEY GENERATION FUNCTIONS
-
-C<diff>, C<LCS>, and C<traverse_sequences> accept an optional last parameter.
-This is a CODE reference to a key generating (hashing) function that should
-return a string that uniquely identifies a given element.
-It should be the case that if two elements are to be considered equal,
-their keys should be the same (and the other way around).
-If no key generation function is provided, the key will be the
-element as a string.
-
-By default, comparisons will use "eq" and elements will be turned into keys
-using the default stringizing operator '""'.
-
-Where this is important is when you're comparing something other than
-strings. If it is the case that you have multiple different objects 
-that should be considered to be equal, you should supply a key
-generation function. Otherwise, you have to make sure that your arrays
-contain unique references.
-
-For instance, consider this example:
-
-  package Person;
-
-  sub new
+  sub someFunkyComparisonFunction
   {
-    my $package = shift;
-    return bless { name => '', ssn => '', @_ }, $package;
+  	my ($a, $b) = @_;
+	$a =~ m{$b};
   }
 
-  sub clone
-  {
-    my $old = shift;
-    my $new = bless { %$old }, ref($old);
-  }
+  @diffs = diff( \@lines, \@patterns, \&someFunkyComparisonFunction );
 
-  sub hash
-  {
-    return shift()->{'ssn'};
-  }
+which would allow you to diff an array @lines which consists of text
+lines with an array @patterns which consists of regular expressions.
 
-  my $person1 = Person->new( name => 'Joe', ssn => '123-45-6789' );
-  my $person2 = Person->new( name => 'Mary', ssn => '123-47-0000' );
-  my $person3 = Person->new( name => 'Pete', ssn => '999-45-2222' );
-  my $person4 = Person->new( name => 'Peggy', ssn => '123-45-9999' );
-  my $person5 = Person->new( name => 'Frank', ssn => '000-45-9999' );
-
-If you did this:
-
-  my $array1 = [ $person1, $person2, $person4 ];
-  my $array2 = [ $person1, $person3, $person4, $person5 ];
-  Algorithm::Diff::diff( $array1, $array2 );
-
-everything would work out OK (each of the objects would be converted
-into a string like "Person=HASH(0x82425b0)" for comparison).
-
-But if you did this:
-
-  my $array1 = [ $person1, $person2, $person4 ];
-  my $array2 = [ $person1, $person3, $person4->clone(), $person5 ];
-  Algorithm::Diff::diff( $array1, $array2 );
-
-$person4 and $person4->clone() (which have the same name and SSN)
-would be seen as different objects. If you wanted them to be considered
-equivalent, you would have to pass in a key generation function:
-
-  my $array1 = [ $person1, $person2, $person4 ];
-  my $array2 = [ $person1, $person3, $person4->clone(), $person5 ];
-  Algorithm::Diff::diff( $array1, $array2, \&Person::hash );
-
-This would use the 'ssn' field in each Person as a comparison key, and
-so would consider $person4 and $person4->clone() as equal.
-
-You may also pass additional parameters to the key generation function
-if you wish.
-
-=head1 AUTHOR
-
-This version by Ned Konz, perl@bike-nomad.com
-
-=head1 CREDITS
-
-Versions through 0.59 (and much of this documentation) were written by:
-
-Mark-Jason Dominus, mjd-perl-diff@plover.com
-
-This version borrows the documentation and names of the routines
-from Mark-Jason's, but has all new code in Diff.pm.
-
-This code was adapted from the Smalltalk code of
-Mario Wolczko <mario@wolczko.com>, which is available at
-ftp://st.cs.uiuc.edu/pub/Smalltalk/MANCHESTER/manchester/4.0/diff.st
-
-The algorithm is that described in 
-I<A Fast Algorithm for Computing Longest Common Subsequences>,
-CACM, vol.20, no.5, pp.350-353, May 1977, with a few
-minor improvements to improve the speed.
+This is actually the reason I wrote this version -- there is no way
+to do this with a key generation function as in the stock Algorithm::Diff.
 
 =cut
 
